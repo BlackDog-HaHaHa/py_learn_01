f61bf422d4ab9c56513f9851a627b651c1aa1bdf
BlackDog-HaHaHa <3089476870@qq.com>
2020-06-15 20:02:08 +0800

    0615

---
+++ testCopy.py
@@ -0,0 +1,31 @@
+# encoding=utf-8
+
+# 浅复制
+
+import copy
+
+object1 = ["will", 28, ["python", "c#", "JavaScript"]]
+object2 = copy.copy(object1)
+
+print(f"id of objectl {id(object1)}")
+print(object1)
+print([id(ele) for ele in object1])
+
+print(f"id of object2 {id(object2)}")
+print(object2)
+print([id(ele) for ele in object2])
+
+# 尝试修改object1，然后看object2的变化
+
+object1[0] = "wilber"
+object1[2].append("CSS")
+
+print("更改后的变化")
+
+print(f"id of objectl {id(object1)}")
+print(object1)
+print([id(ele) for ele in object1])
+
+print(f"id of object2 {id(object2)}")
+print(object2)
+print([id(ele) for ele in object2])
