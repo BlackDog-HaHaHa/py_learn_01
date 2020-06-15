3e53aa45e8a8bae8ced1b6311f9a99852a1435f2
BlackDog-HaHaHa <3089476870@qq.com>
2020-06-15 19:09:06 +0800

    Initial commit

---
+++ testFuzhi.py
@@ -0,0 +1,29 @@
+# encoding=utf-8
+
+# 对象赋值操作
+
+object1 = ["will", 28, ["python", "c#", "JavaScript"]]
+object2 = object1
+
+print(f"id of objectl {id(object1)}")
+print(object1)
+print([id(ele) for ele in object1])
+
+print(f"id of object2 {id(object2)}")
+print(object2)
+print([id(ele) for ele in object2])
+
+# 尝试修改object1，然后看object2的变化
+
+object1[0] = "wilber"
+object1[2].append("CSS")
+
+print("更改后的变化")
+
+print(f"id of objectl {id(object1)}")
+print(object1)
+print([id(ele) for ele in object1])
+
+print(f"id of object2 {id(object2)}")
+print(object2)
+print([id(ele) for ele in object2])
