afba81aa87d8a080552520641e09dc14c039c73c
BlackDog-HaHaHa <3089476870@qq.com>
2020-06-22 21:01:16 +0800

    0622

--- psutil_test.py
+++ psutil_test.py
@@ -3,14 +3,29 @@ import psutil
 import signal
 
 # 按名称查找进程相关信息 1
-def find_procs_by_name(name):
+def find_procs_by_name_1(name):
     "Return a list of processes matching 'name'."
     ls = []
     for p in psutil.process_iter(attrs=['name']):
         # print(p)
+        # print(p.info['name'])
         if p.info['name'] == name:
             ls.append(p)
     # print(ls)
     return ls
 
-find_procs_by_name("TIM.exe")
+# a = find_procs_by_name_1('TIM.exe')
+# print(a)
+
+
+def find_procs_by_name_2(name):
+    ls = []
+    for p in psutil.process_iter(attrs=["name","exe","cmdline"]):
+        if name == p.info['name'] or \
+            p.info['exe'] and os.path.basename(p.info['exe']) == name or \
+            p.info['cmdline'] and p.info['cmdline'][0] == name:
+            ls.append(p)
+    return ls
+
+# a = find_procs_by_name_2('TIM.exe')
+# print(a)
